9c3bf3e149856ece32cf653ffaa0b56acec8dca1
Bharath Ram J <144774795+bharathvj2004@users.noreply.github.com>
2023-09-22 12:19:00 +0530

    Update challenge 2 cricket.py
    Program name wrong

--- challenge 2 cricket.py
+++ challenge 2 cricket.py
@@ -1,8 +1,27 @@
-def checkYear(year):
-    import calendar
-    return(calendar.isleap(year))
-year = 2000
-if (checkYear(year)):
-    print("Leap Year")
-else:
-    print("Not a Leap Year")
+
+class Player:
+
+    def play(self):
+
+        print("The player is playing cricket.")
+
+
+class Batsman(Player):
+
+    def play(self):
+
+        print("The batsman is batting.")
+
+class Bowler(Player):
+
+    def play(self):
+
+        print("The bowler is bowling.")
+
+batsman = Batsman()
+
+bowler = Bowler()
+
+batsman.play()
+
+bowler.play()
