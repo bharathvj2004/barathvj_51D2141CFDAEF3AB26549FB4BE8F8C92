9f98a143211aa6e9e16b4152e969bc816e0244a1
Bharath Ram J <144774795+bharathvj2004@users.noreply.github.com>
2023-09-22 12:22:52 +0530

    Update challenge 2 bank.py
    Program wrong

--- challenge 2 bank.py
+++ challenge 2 bank.py
@@ -1,26 +1,24 @@
-class Player:
-
-    def play(self):
-
-        print("The player is playing cricket.")
-
-
-class Batsman(Player):
-
-    def play(self):
-
-        print("The batsman is batting.")
-
-class Bowler(Player):
-
-    def play(self):
-
-        print("The bowler is bowling.")
-
-batsman = Batsman()
-
-bowler = Bowler()
-
-batsman.play()
-
-bowler.play()
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+ 
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+s = Bank_Account()
+s.deposit()
+s.withdraw()
+s.display()
